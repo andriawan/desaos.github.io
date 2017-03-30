a8c7c0e3751f25e5e84f6bbf02b351190535bafe
razaqultegar <razaqul@puskomedia.id>
2017-03-30 23:06:40 +0700

    update template

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
